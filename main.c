63a7449e4261bcb5d887eb3f6062a1f83b2dd6d3
Ritvik <youritvik02@gmail.com>
2022-10-11 15:47:04 -0400

    import hw files

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * COP 3515 – Fall Semester 2022
+ *
+ * Homework #2: Spotting The Hacker
+ *
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+int main(void) {
+  
+  return 0;
+}
