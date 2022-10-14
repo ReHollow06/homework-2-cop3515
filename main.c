855ca790d37548f90a446dfce14aa78db637091c
Ritvik <youritvik02@gmail.com>
2022-10-14 16:15:57 -0400

    implemented parity checking function

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 #define NUM_FILES 3
 
 int decToBin(int decNum);
-bool hasEvenParity(int binNum);
+bool hasEvenParity(int binNum, int);
 
 int decToBin(int decNum){ // converts decimal number to binary 
   int binaryNumArray[8] = {0,0,0,0,0,0,0,0};
@@ -36,7 +36,30 @@ int decToBin(int decNum){ // converts decimal number to binary
 }
 
 bool hasEvenParity(int binNum, int parityBit){
-  return true;
+  int numOnes = parityBit;
+  while (binNum != 0)
+  {
+    if (binNum % 10 == 1)
+    {
+      numOnes++;
+    }
+    else
+    {
+      numOnes += 0;
+    }
+    binNum /= 10;
+  }
+
+  printf("numOnes: %d\n", numOnes);
+
+  if (numOnes % 2 == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 int main(void)
@@ -47,8 +70,8 @@ int main(void)
   dataFiles[2] = fopen("White House Transmission Data - Checksum.txt", "r");
 
   // File open test
-  for (int i = 0; i < NUM_FILES; i++)
-  { // checks for null pointers in all files
+  for (int i = 0; i < NUM_FILES; i++) // checks for null pointers in all files
+  { 
     if (dataFiles[i] == NULL)
     {
       printf("Couldn't open file %d\n", i + 1);
@@ -65,20 +88,43 @@ int main(void)
 
   printf("** Part 1 - Parity Check Processing\n\n\n");
   int dataBytes[8];
-  int parityByte[8] = {0,0,0,0,0,0,0,0};
-  // fscanf(dataFiles[0], "%3d%4d%4d%4d%4d%4d%4d%4d%4d", &dataBytes[0], &dataBytes[1], &dataBytes[2], &dataBytes[3], &dataBytes[4], &dataBytes[5], &dataBytes[6], &dataBytes[7], &parityByte);
-  // for (int i = 0; i < 8; i++)
-  // {
-  //   printf("%d", dataBytes[i]);
-  // }
-  // printf("\n");
-  // printf("%d", parityByte);
-
-  while (!feof(dataFiles[0]))
-  {
-    printf("line\n");
-  }
+  int parityByte;
+  int parityByteBinArray[8] = {0,0,0,0,0,0,0,0};
+  fscanf(dataFiles[0], "%3d%4d%4d%4d%4d%4d%4d%4d%4d", &dataBytes[0], &dataBytes[1], &dataBytes[2], &dataBytes[3], &dataBytes[4], &dataBytes[5], &dataBytes[6], &dataBytes[7], &parityByte);
+  int parityByteBin = decToBin(parityByte);
   
+  for (int i = 8; i > 0; i--)
+  {
+    parityByteBinArray[i - 1] = parityByteBin % 10;
+    parityByteBin /= 10; 
+  }
+
+  printf("\nData stream:");
+
+  for (int i = 0; i < 8; i++)
+  {
+    printf("%2d", dataBytes[i]);
+  }
+  printf("\n\nParity Byte: %d", parityByte);
+  printf("\n\n");
+
+  for (int i = 0; i < 8; i++)
+  {
+    int binData = decToBin(dataBytes[i]);
+    printf("Data Item: %d, Binary: %d, Parity Bit: %d\n", dataBytes[i], binData, parityByteBinArray[i]);
+    if (hasEvenParity(binData, parityByteBinArray[i]) == true)
+    {
+      printf("No error in transmission byte: %d", dataBytes[i]);
+    }
+    else 
+    {
+      printf("Error in transmission byte: %d", dataBytes[i]);
+    }
+    
+    printf("\n\n");
+  }
+
+  // PART 1 - PARITY CHECK PROCESSING END
 
   for (int k = 0; k < NUM_FILES; k++) { // closes all files in file array
     fclose(dataFiles[k]);
